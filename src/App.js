cee4079dcfce2a68b69d09ff2ee8e6b6e8f5dc07
rosgas <rgasbarri.274@gmail.com>
2022-09-12 09:01:06 +1000

    Check current user

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/explore" element={<Explore />} />
           <Route path="/auctions" element={<Auctions />} />
-          <Route path="/profile" element={<SignIn />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
