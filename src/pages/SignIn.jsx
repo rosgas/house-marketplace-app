4da12f1483c562671be9c473e82da55c4927e901
rosgas <rgasbarri.274@gmail.com>
2022-09-11 17:46:57 +1000

    Sign In & Sign Up Forms

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,83 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import logo from "../assets/svg/logo-white.svg";
+import visibilityIcon from "../assets/svg/visibility.svg";
+
 function SignIn() {
+  const [showPassword, setShowPassword] = useState();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
   return (
-    <div>
-      <h1>SignIn</h1>
-    </div>
+    <>
+      <div className="container card-container">
+        <div className="card">
+          <div className="card-image">
+            <img src={logo} width="277px" height="78px" />
+            <span className="attribute">Image attribute</span>
+          </div>
+          <div className="card-form">
+            <header>
+              <p className="title">Wecome back</p>
+            </header>
+            <form>
+              <div className="form-group">
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={onChange}
+                />
+                <label htmlFor="email">Email:</label>
+              </div>
+
+              <div className="form-group password-input">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  value={password}
+                  onChange={onChange}
+                />
+                <label htmlFor="password">Password:</label>
+                <img
+                  src={visibilityIcon}
+                  alt="show password"
+                  className="show-password"
+                  onClick={() => setShowPassword((prevState) => !prevState)}
+                />
+              </div>
+
+              <Link to="/forgot-password" className="link forgot-password-link">
+                Forgot password
+              </Link>
+              <button className="btn btn-main">Login</button>
+            </form>
+
+            {/* Google OAuth */}
+            <div className="sign-up">
+              <p>
+                Not registered?{" "}
+                <Link to="/sign-up" className="link register-link">
+                  Sign up then!
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,85 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import logo from "../assets/svg/logo-white.svg";
+import visibilityIcon from "../assets/svg/visibility.svg";
+
 function SignUp() {
+  const [showPassword, setShowPassword] = useState();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const { email, password, name } = formData;
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
   return (
-    <div>
-      <h1>SignUp</h1>
-    </div>
+    <>
+      <div className="container card-container">
+        <div className="card">
+          <div className="card-image">
+            <img src={logo} width="277px" height="78px" />
+            <span className="attribute">Image attribute</span>
+          </div>
+          <div className="card-form">
+            <header>
+              <p className="title">Join us</p>
+            </header>
+            <form>
+              <div className="form-group">
+                <input type="text" id="name" value={name} onChange={onChange} />
+                <label htmlFor="text">Name:</label>
+              </div>
+              <div className="form-group">
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={onChange}
+                />
+                <label htmlFor="email">Email:</label>
+              </div>
+
+              <div className="form-group password-input">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  value={password}
+                  onChange={onChange}
+                />
+                <label htmlFor="password">Password:</label>
+                <img
+                  src={visibilityIcon}
+                  alt="show password"
+                  className="show-password"
+                  onClick={() => setShowPassword((prevState) => !prevState)}
+                />
+              </div>
+
+              <button className="btn btn-main">Sign up</button>
+            </form>
+
+            {/* Google OAuth */}
+            <div className="sign-up">
+              <p>
+                Already registered?{" "}
+                <Link to="/sign-in" className="link register-link">
+                  Sign in then!
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
